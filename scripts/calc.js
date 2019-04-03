2b1837c060078f33c1b799720dad8315c1a51377
Wicky <wicky.akloe@gmail.com>
2019-04-03 10:50:33 +0000

    defensive programming added error that passes

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,3 +1,7 @@
 function addition(numOne, numTwo){
-    return numOne + numTwo;
+    if(typeof(numOne) == "number" && typeof(numTwo) == "number"){
+        return numOne + numTwo;
+    } else {
+        return "Error!";
+    }
 }  
--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -5,6 +5,9 @@ describe("Calculator", function() {
      });
      it("should return 26", function() {
          expect(addition(7, 19)).toBe(26);
+     });
+     it("should return an error if we dont suplly two numbers", function() {
+         expect(addition("Hitchhikers", "Guide")).toBe("Error!");
      })
  });
 });
